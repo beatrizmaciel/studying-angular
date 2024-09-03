87ff71e7bf09525c22ebbb55e788de1c8a6f9ece
beatriz <beatriz.maciel@vilt-group.com>
2024-09-03 18:13:25 +0100

    :sparkles: adding new components

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,11 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  function minhaFunc(x: any,y: any) {
+    return x+y;
+  }
+
+  // ES 6 ou ES 2015
+  let num = 2;
+  const PI = 3.14;
