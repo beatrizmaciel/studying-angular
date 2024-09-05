42978ce1fb3c1916c5e3e14ad5faaf53d407c5e9
beatriz <beatriz.maciel@vilt-group.com>
2024-09-05 16:38:45 +0100

    :sparkles: creating new module and new cursos component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MeuPrimeiroComponente } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { CursosComponent } from './cursos/cursos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MeuPrimeiroComponente, MeuPrimeiro2Component],
+  imports: [RouterOutlet, MeuPrimeiroComponente, MeuPrimeiro2Component, CursosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
