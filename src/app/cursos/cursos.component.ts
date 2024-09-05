0ce0d778bc8045a7a438dd7a180bf0065d049994
beatriz <beatriz.maciel@vilt-group.com>
2024-09-05 16:57:15 +0100

    :construction: learning interpolation

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [],
+  imports: [CursosDetalheComponent],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
 export class CursosComponent {
+
+  nomePortal: string;
+
+  constructor(){
+    this.nomePortal = 'Portal de Cursos';
+  }
 
 }
