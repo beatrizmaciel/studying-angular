e5198b01d5ce08c88315be4583387bb5e5e345d5
beatriz <beatriz.maciel@vilt-group.com>
2024-09-05 17:13:34 +0100

    :construction: learning *ngFor

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { CursosDetalheComponent } from './cursos-detalhe/cursos-detalhe.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cursos',
   standalone: true,
-  imports: [CursosDetalheComponent],
+  imports: [CommonModule, CursosDetalheComponent],
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
 export class CursosComponent {
 
   nomePortal: string;
+
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
 
   constructor(){
     this.nomePortal = 'Portal de Cursos';
